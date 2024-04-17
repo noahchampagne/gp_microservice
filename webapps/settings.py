076bd9bb711987e88630aff58be1ea01409d6f81
Noah Champagne <nchampag@andrew.cmu.edu>
2024-04-16 21:00:03 -0400

    adding hosts

--- webapps/settings.py
+++ webapps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2i@v&%%)k%3*az3%sgmvg8y&lx!r4_b*pll8pcs0+m7k7t1^=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
