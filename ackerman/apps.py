5be52c68c32b2c537000553dcfcabc2b0c30a064
Noah Champagne <nchampag@andrew.cmu.edu>
2024-04-16 20:51:05 -0400

    ACKERMAN

---
+++ ackerman/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AckermanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ackerman'
---
+++ ackerman/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+calls = 0
+
+def start_action(request):
+    #If initial request
+    if request.method == "GET":
+      num = request.GET.get('num')
+      result = ackermann(3, int(num))
+      print(result)
+      context = {'res': result}
+      return render(request, "start.html", context)
+
+def ackerman_fn(m, n):
+  global calls
+  calls += 1
+  if m == 0:
+      return n + 1
+  elif n == 0:
+      return ackerman_fn(m - 1, 1)
+  else:
+      return ackerman_fn(m - 1, ackerman_fn(m, n - 1))
+
+def ackermann(m, n):
+    global calls
+    stack = []
+
+    while True:
+        calls += 1
+        if m == 0:
+            if not stack:
+                return n + 1
+            m, n = stack.pop(), n + 1
+        elif n == 0:
+            m, n = m - 1, 1
+        else:
+            stack.append(m - 1)
+            n -= 1
